a2847b974b89f2eb1df0f6de46f230d5ccec26bc
Prarthana M Kunder <120232359+Prartz14@users.noreply.github.com>
2023-08-04 18:41:02 +0530

    Add files via upload

---
+++ day5/Ascii_ShiftArray2.java
@@ -0,0 +1,33 @@
+package day5;
+import java.util.*;
+public class Ascii_ShiftArray2 {
+	
+	public static String ShiftString(String s,int[] shift)
+	{
+		String newString="";
+		char shiftedchar;
+		for(int i=0;i<shift.length;i++)
+		{
+			int shifts=0,unicode=0;
+			unicode=s.charAt(i);
+			for(int j=i;j<shift.length;j++)
+				shifts+=shift[j];
+			if((unicode+shifts)/123>0)
+				shiftedchar=(char)((unicode+shifts)%123+97);
+			else
+				shiftedchar=(char)(unicode+shifts);
+			newString+=shiftedchar;
+		}
+		return (String)newString;
+	}
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		int[] shift=new int[s.length()];
+		for(int i=0;i<s.length();i++)
+			shift[i]=sc.nextInt();
+		String shiftedString=ShiftString(s,shift);
+		System.out.println(shiftedString);
+	}
+
+}
